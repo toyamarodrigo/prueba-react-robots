02a4825c06cada84a3bf0bfbe9c021a95fc50a19
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-18 01:03:57 -0300

    added search box

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -20,9 +21,21 @@ export class App extends Component {
   }
 
   render() {
+    // const monster = this.state.monsters;
+    // const searchFields = this.state.searchFields;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
