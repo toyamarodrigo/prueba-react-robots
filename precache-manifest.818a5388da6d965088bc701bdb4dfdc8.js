7d9484c76bc2dfe396127f6ffa2cb08deb95592e
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-18 20:01:52 -0300

    Updates

---
+++ precache-manifest.818a5388da6d965088bc701bdb4dfdc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02e8fee6dfc4b275b1c124ad1a5348d",
+    "url": "/prueba-react-robots/index.html"
+  },
+  {
+    "revision": "7cfb64561bebe598c15e",
+    "url": "/prueba-react-robots/static/css/main.f35d716d.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/prueba-react-robots/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba-react-robots/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfb64561bebe598c15e",
+    "url": "/prueba-react-robots/static/js/main.734ecf18.chunk.js"
+  },
+  {
+    "revision": "085ef4c50475fc03673f",
+    "url": "/prueba-react-robots/static/js/runtime-main.657067be.js"
+  }
+]);
